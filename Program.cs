8cf777938f8779c2fe1f00851ac311bc66aee5da
Edu <eduardokaue277@gmail.com>
2024-02-27 00:05:03 -0300

    fiXED

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace testesAula01
+{
+    
+    class Testes
+    {
+            static void Main(string[] args) 
+            {
+                detalharData();
+
+            }
+
+            public static void detalharData()
+            {
+                Console.WriteLine("Digite uma data: ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                string frase1 = string.Format("Dia da semana: {0:dddd}, Mês: {0:MMMM}, São: {1:HH:mm}", date, DateTime.Now);
+                string frase2 = string.Format("Dia da semana: {0:dddd}, Mês: {0:MMMM}", date);
+
+                if(date.DayOfWeek == DayOfWeek.Sunday)
+                Console.WriteLine(frase1);
+
+                else
+                Console.WriteLine(frase2);
+
+                
+    
+
+
+
+
+
+                
+
+            }
+
+
+
+    }
+
+
+}
